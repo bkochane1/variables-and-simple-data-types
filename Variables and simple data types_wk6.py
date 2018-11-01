df00753fef2ef61289cd0f67c23b320195f3739a
bkochane1 <44319159+bkochane1@users.noreply.github.com>
2018-10-31 21:15:56 -0400

    Add files via upload

---
+++ Variables and simple data types_wk6.py
@@ -0,0 +1,36 @@
+first_name = "beata " # varaible first name
+last_name = "KOCHANEK" # variable last name 
+greeting = "Hello, " # variable set to show hello 
+print(greeting + "\n\n" + first_name.upper() + last_name.lower()) # show hello and add two new lines, show first name in uppercase and last name lowercase
+
+string_quote = "\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible\"- Francis of Assisi" # print quote with quotes on both sides of the text
+print(string_quote)
+
+numberOne= 12.2
+numberTwo = 2.1
+addition_result = numberOne + numberTwo # add numbers
+print(addition_result) # show the result of addition
+
+subtract_result = numberOne - numberTwo # subtract numbers
+print(subtract_result) # show the result of addition
+
+multiply_result = numberOne * numberTwo # multiply numbers
+print(multiply_result) # show the result of multiplication
+
+divide_result = numberOne / numberTwo #divide numbers
+print(divide_result) # show the result of division
+
+
+string_variable = "October " # store current month as variable
+number_variable = 29 # store day of the month 
+output_variable = "\n\t\tToday is day " + str(number_variable) + " " + "of the month of " + string_variable # output the message on a new line with two tabs
+
+print(output_variable) # show the output variable 
+
+
+
+
+
+
+
+
